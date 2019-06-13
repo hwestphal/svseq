58314207e8bcd1f456ab296aef2446dbed7366bd
Harald Westphal <h.westphal@gmx.de>
2019-06-13 19:16:30 +0200

    Add tempo panel

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
-from launchpad import Launchpad, BUTTON_MIXER, BUTTON_SESSION, BUTTON_USER_1, BUTTON_USER_2, BUTTON_UP, BUTTON_DOWN
+from launchpad import Launchpad, BUTTON_MIXER, BUTTON_SESSION, BUTTON_LEFT, BUTTON_USER_1, BUTTON_USER_2, BUTTON_UP, BUTTON_DOWN
 from padget import Padget
 from .mixer import Mixer
 from .session import Session
+from .tempo import Tempo
 from typing import Optional
 
 
@@ -9,23 +10,32 @@ class App(Padget):
 
     def __init__(self, pad: Launchpad):
         super().__init__(pad)
-        self.__mixer: Optional[Mixer] = None
         self.__session: Optional[Session] = Session(pad)
+        self.__mixer: Optional[Mixer] = None
+        self.__tempo: Optional[Tempo] = None
 
     def _buttonPressed(self, i: int) -> bool:
         if i == BUTTON_MIXER and not self.__mixer:
             self.__mixer = Mixer(self._pad)
             self.__session = None
+            self.__tempo = None
             return True
         if i == BUTTON_SESSION and not self.__session:
-            self.__mixer = None
             self.__session = Session(self._pad)
+            self.__mixer = None
+            self.__tempo = None
+            return True
+        if i == BUTTON_LEFT and not self.__tempo:
+            self.__tempo = Tempo(self._pad)
+            self.__mixer = None
+            self.__session = None
             return True
         return False
 
     def _render(self) -> None:
         self._pad.set(BUTTON_MIXER, 0x030)
         self._pad.set(BUTTON_SESSION, 0x030)
+        self._pad.set(BUTTON_LEFT, 0x030)
         self._pad.set(BUTTON_USER_1, 0x000)
         self._pad.set(BUTTON_USER_2, 0x000)
         self._pad.set(BUTTON_UP, 0x000)
---
+++ app/tempo.py
@@ -0,0 +1,155 @@
+from launchpad import Launchpad, BUTTON_LEFT, BUTTON_SCENE_1, BUTTON_UP, BUTTON_DOWN
+from padget import Padget
+from typing import List
+from .project import project
+
+
+class Tempo(Padget):
+
+    def __init__(self, pad: Launchpad):
+        super().__init__(pad)
+
+    def _buttonPressed(self, i: int) -> bool:
+        if i == BUTTON_UP and project.tempo < 240:
+            project.tempo += 1
+            return True
+        if i == BUTTON_DOWN and project.tempo > 40:
+            project.tempo -= 1
+            return True
+        if i == BUTTON_SCENE_1 and project.tempo <= 230:
+            project.tempo += 10
+            return True
+        if i == BUTTON_SCENE_1+1 and project.tempo >= 50:
+            project.tempo -= 10
+            return True
+        return False
+
+    def _render(self) -> None:
+        self._pad.set(BUTTON_LEFT, 0x033)
+        self._pad.set(BUTTON_SCENE_1, 0x030 if project.tempo <= 230 else 0x000)
+        self._pad.set(BUTTON_SCENE_1 + 1,
+                      0x030 if project.tempo >= 50 else 0x000)
+        for i in range(2, 8):
+            self._pad.set(BUTTON_SCENE_1 + i, 0x000)
+        self._pad.set(BUTTON_UP, 0x030 if project.tempo < 240 else 0x000)
+        self._pad.set(BUTTON_DOWN, 0x030 if project.tempo > 40 else 0x000)
+        for i in range(16):
+            self._pad.set(i, 0x000)
+
+        d = _DIGITS_2[project.tempo // 100]
+        for i in range(5):
+            for j in range(2):
+                self._pad.set(8 + i * 8 + j, 0x033 if d[i][j] else 0x000)
+
+        d = _DIGITS_3[(project.tempo // 10) % 10]
+        for i in range(5):
+            for j in range(3):
+                self._pad.set(8 + i * 8 + j + 2, 0x030 if d[i][j] else 0x000)
+
+        d = _DIGITS_3[project.tempo % 10]
+        for i in range(5):
+            for j in range(3):
+                self._pad.set(8 + i * 8 + j + 5, 0x003 if d[i][j] else 0x000)
+
+        for i in range(56, 64):
+            self._pad.set(i, 0x000)
+
+
+def _to_data(data: str, width: int, height: int = 5) -> List[List[List[int]]]:
+    lines = data.splitlines()[1:]
+    assert len(lines) % (height + 1) == height
+    chars = []
+    while lines:
+        rows = []
+        for i in range(height):
+            line = (lines[i] + ' ' * width)[:width]
+            row = []
+            for c in line:
+                row.append(1 if c == '*' else 0)
+            rows.append(row)
+        chars.append(rows)
+        lines = lines[height+1:]
+    return chars
+
+
+_DIGITS_2 = _to_data("""
+
+
+
+
+
+
+*
+*
+*
+*
+*
+
+**
+ *
+**
+*
+**
+""", 2)
+
+_DIGITS_3 = _to_data("""
+***
+* *
+* *
+* *
+***
+
+ *
+ *
+ *
+ *
+ *
+
+***
+  *
+***
+*
+***
+
+***
+  *
+***
+  *
+***
+
+* *
+* *
+***
+  *
+  *
+
+***
+*
+***
+  *
+***
+
+***
+*
+***
+* *
+***
+
+***
+  *
+  *
+  *
+  *
+
+***
+* *
+***
+* *
+***
+
+***
+* *
+***
+  *
+***
+""", 3)
